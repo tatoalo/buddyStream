89011091f3b3e942a14ba9a9f351f59267fd40f1
tatoalo <apogliaghi@gmail.com>
2021-01-30 17:46:50 +0100

    Session Cookies support

--- buddyStream.py
+++ buddyStream.py
@@ -1,7 +1,7 @@
+import _pickle as pickle
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
 import time
 
 
@@ -14,9 +14,16 @@ def init_chrome():
     opt_args.add_argument("--disable-gpu")
 
     browser = webdriver.Chrome(options=opt_args)
+
+    cookies = import_session_cookies(browser)
+    time.sleep(2)
     browser.get("https://app.buddyfit.club/classes/replay/8a1d6bc3-0273-4b66-96f5-326d8ab23bf2/live")
 
-    return browser
+    return browser, cookies
+
+
+def close_browser(b):
+    b.close()
 
 
 def extract_keys():
@@ -36,18 +43,17 @@ def extract_keys():
         print("*** file doesn't exists, creating 'keys'"
               " file, now fill it with data! ... exiting ***")
         open("keys", 'w')
-        return -1
+        close_browser()
 
 
 def logged_checker(b):
     try:
-        time.sleep(1)
-        # b.find_element(By.XPATH, '//button[text()="Entra"]')
+        # time.sleep(1)
         login_button = b.find_element_by_xpath("//button[@class='button is-medium is-fullwidth is-rounded is-primary']")
         handle_login(b, login_button)
 
     except NoSuchElementException:
-        print("Login needed but could not find button")
+        print("Login needed but could not find button!")
 
 
 def handle_login(b, button):
@@ -62,16 +68,51 @@ def handle_login(b, button):
 
     button.click()
 
+    time.sleep(5)
+
+    try:
+        expecting_nothing = b.find_element_by_xpath("//button[@class='button is-medium is-fullwidth is-rounded is-primary']")
+        print("Login Failed")
+        close_browser(b)
+    except NoSuchElementException:
+        print("Logged In!")
+        export_session_cookies(b)
+
+
+def export_session_cookies(b):
+    with open("cookies", 'wb') as f:
+        pickle.dump(b.get_cookies(), f)
+
+    print("cookies saved")
+
+
+def import_session_cookies(b):
+    try:
+        with open("cookies", 'rb') as ck:
+            session_cookies = pickle.load(ck)
+            # Forcing cookie domain
+            b.get("https://app.buddyfit.club")
+            for c in session_cookies:
+                b.add_cookie(c)
+
+        print("Logged in via session-saved cookies")
+        return True
+
+    except FileNotFoundError:
+        print("*** cookies session file NOT found ***")
+        return False
+
 
 def main():
-    b = init_chrome()
+    b, cookies_flag = init_chrome()
 
-    logged_checker(b)
+    if not cookies_flag:
+        logged_checker(b)
 
     if input("Quit?") == "Y":
-        b.close()
+        close_browser(b)
     else:
-        b.close()
+        close_browser(b)
 
 
 if __name__ == "__main__":
